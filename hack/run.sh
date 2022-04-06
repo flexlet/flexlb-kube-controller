151fd383be67dce1f18c87f4ffe5c9ab3d6db4b6
王耀辉 <yaohuiwang@outlook.com>
2022-04-06 15:41:35 +0000

    update readme

--- hack/run.sh
+++ hack/run.sh
@@ -12,6 +12,5 @@ export FLEXLB_TLS_CLIENT_CERT=../certs/client.crt
 export FLEXLB_TLS_CLIENT_KEY=../certs/client.key
 export FLEXLB_REFRESH_INTERVAL=30
 export FLEXLB_NAMESPACE=kube-system
-export FLEXLB_TRAFFIC_NETWORK=192.168.1.0/24
 
 make run
